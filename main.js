b9c7e6d0db775782ecbbbf556840354c92717661
Brandon <bjcompto6@gmail.com>
2024-02-04 09:22:10 -0800

    updated build

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ class Field {
         this.gameField = gameField; 
     }
 
+    newGame() {
+        console.log(this.gameField); 
+    }
+    
     printBoard() {
         setTimeout(() => {
             let twoArrToStr = this.gameField.map(arr => arr.join('')).join('');
@@ -22,8 +26,20 @@ class Field {
         const userMove = prompt('Which direction would you like to move? ');
         console.log(`You want to move: ${userMove}`); 
         this.printBoard();
+        this.holePresent();
     }
 
+    holePresent() {
+        for(let i = 0; i < this.gameField.length; i++) {
+            for(let j = 0; j < this.gameField[i].length; j++) {
+                if(this.gameField[i][j] === this.hole) {
+                    console.log('There is a hole here.')
+                    this.newGame();
+                    break; 
+                }
+            }
+        }
+    }
 
 }
 
